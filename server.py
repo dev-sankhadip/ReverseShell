efcf8337991452648f5206fc04497ba5f12d8393
sankhadip samanta <sankhsamanta.cse17@chitkarauniversity.edu.in>
2020-01-10 19:40:29 +0530

    code: send command to client

--- server.py
+++ server.py
@@ -32,3 +32,21 @@ def socket_accept():
     print("Socket connection established | IP "+address[0]+" | PORT "+address[1])
     send_command(conn)
     conn.close()
+
+def send_command(conn):
+    while True:
+        cmd = input()
+        if cmd=='quit':
+            conn.close()
+            s.close()
+            sys.exit()
+        if len(str.encode(cmd))>0:
+            conn.send(str.encode(cmd))
+            client_response = str(conn.recv(1024),"utf-8")
+            print(client_response, end="")
+
+
+if __name__ == "__main__":
+    create_socket()
+    bind_socket()
+    socket_accept()
